b42c662adf019dfdf7808195d227b34dd7efa977
Ajit <Ajit@DESKTOP-CEE9BMK>
2018-06-10 14:38:07 +0530

    my first java project

--- TechProject/src/Ajit.java
+++ TechProject/src/Ajit.java
@@ -1,5 +1,6 @@
 public class Ajit
-{public static void main(String[]args)
+{
+	public static void main(String[]args)
 	{System.out.println("hello");
 	}
 	}
---
+++ TechProject/src/Bhai.java
@@ -0,0 +1,8 @@
+
+public class Bhai {
+	public static void main(String[] args) {
+		System.out.println("bhai is king");
+
+	}
+
+}
